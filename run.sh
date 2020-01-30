427c8b21c9f5fc4a6610e9975470cc26e930c4a5
Simon Oliver <simon.oliver@ga.gov.au>
2020-01-31 09:30:38 +1100

    shell update

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-rm -rf /home/ec2-user/hotspots_animation/frames* >> /home/ec2-user/hotspots_animation/cronlog.log 2>&1
+rm -rf /home/ec2-user/hotspots_animation/frames* >> /home/ec2-user/hotspots_animation/cronlog.log
 
 source /home/ec2-user/miniconda3/bin/activate /home/ec2-user/miniconda3/envs/animation
 
 cd /home/ec2-user/hotspots_animation
-python DEA_Hotspots_animations.py >> /home/ec2-user/hotspots_animation/cronlog.log 2>&1
+python DEA_Hotspots_animations.py >> /home/ec2-user/hotspots_animation/cronlog.log
 
-for i in `find . -name '*mp4'`; do aws s3 cp $i s3://hotspots-animation/ --acl public-read; done >> /home/ec2-user/hotspots_animation/cronlog.log 2>&1
-for i in `find . -name '*gif'`; do aws s3 cp $i s3://hotspots-animation/ --acl public-read; done >> /home/ec2-user/hotspots_animation/cronlog.log 2>&1
+for i in `find . -name '*mp4'`; do aws s3 cp $i s3://hotspots-animation/ --acl public-read; done >> /home/ec2-user/hotspots_animation/cronlog.log
+for i in `find . -name '*gif'`; do aws s3 cp $i s3://hotspots-animation/ --acl public-read; done >> /home/ec2-user/hotspots_animation/cronlog.log
